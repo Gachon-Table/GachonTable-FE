d655e7cb506901369595c5d418280b1e70ef0744
jodan <wdd789q@naver.com>
2024-05-19 16:44:14 +0900

    fix : merge 시 나타내는 에러 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    swcMinify: true, // Next.js에서 SWC를 사용하여 최적화합니다.
+    compiler: {
+      styledComponents: true, // SWC 컴파일러를 통해 styled-components를 사용합니다.
+    },
+  };
+  
+  export default nextConfig;
+  
