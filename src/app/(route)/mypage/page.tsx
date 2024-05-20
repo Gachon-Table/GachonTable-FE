432c54d4d85ea92edbd216b1399cb9d852488637
ggome1 <bookincar@naver.com>
2024-05-20 13:30:54 +0900

    style: 마이페이지 UI 구현

--- src/app/(route)/mypage/page.tsx
+++ src/app/(route)/mypage/page.tsx
@@ -8,13 +8,32 @@ const Mypage = () => {
   const { data: session } = useSession();
   console.log(session);
   return (
-    <div onClick={() => signIn('kakao', { callbackUrl: '/mypage' })}>
-      <Image
-        src="/images/kakao_login_medium_wide.png"
-        alt="kakao-login"
-        width={300}
-        height={50}
-      />
+    <div>
+      <div className='flex'>
+        <div></div>
+        <div className='flex flex-col'>
+          <div>로그인하지 않은 상태입니다. <br /> 로그인 후 이용해 주세요.</div>
+          <div onClick={() => signIn('kakao', { callbackUrl: '/mypage' })}>
+            <Image
+              src="/images/kakao_login_medium_wide.png"
+              alt="kakao-login"
+              width={300}
+              height={50}
+            />
+          </div>
+        </div>
+      </div>
+      <div className='flex'>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 };
