0ce4cf96dcd3c32d41bc3e5c05ccc255d7adf3e5
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-05-22 02:22:58 +0900

    feat: 주점 관리 페이지 기본 틀 구현

---
+++ src/app/(route)/admin/pub-management/page.tsx
@@ -0,0 +1,7 @@
+export default function PubManagement() {
+  return (
+    <div className="bg- flex min-h-screen flex-col items-center justify-center bg-deep-cove">
+      <h1>주점 관리</h1>
+    </div>
+  );
+}
