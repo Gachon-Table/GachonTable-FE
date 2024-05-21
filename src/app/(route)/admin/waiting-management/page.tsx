5436bea245230757e153d594340ba86dd083560f
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-05-22 02:23:10 +0900

    feat: 대기 인원 관리 페이지 기본 틀 구현

---
+++ src/app/(route)/admin/waiting-management/page.tsx
@@ -0,0 +1,7 @@
+export default function WaitingManagement() {
+  return (
+    <div className="bg- flex min-h-screen flex-col items-center justify-center bg-deep-cove">
+      <h1>대기 인원 관리</h1>
+    </div>
+  );
+}
