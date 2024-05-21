df96f799693d73ad1769affd9b61db24538a617e
ggome1 <bookincar@naver.com>
2024-05-21 21:35:39 +0900

    style: 마이페이지 UI

---
+++ src/app/(route)/mypage/_components/waited/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Waited = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Waited;
--- src/app/(route)/mypage/_components/waiting/page.tsx
+++ src/app/(route)/mypage/_components/waiting/page.tsx
@@ -1,14 +1,17 @@
-import * as React from 'react';
+import React from 'react';
 
-export interface IAppProps {
-}
-
-export default class App extends React.Component<IAppProps> {
-  public render() {
-    return (
-      <div>
-        
+const Waiting = () => {
+  return (
+    <div className="flex w-[100%] flex-col">
+      <div className="flex items-center justify-evenly border-b-2">
+        <div>주점이름</div>
+        <div>
+          <div>방문 입장 시간 : 19:00</div>
+          <div>대기 소요 시간 : 1시간</div>
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Waiting;
