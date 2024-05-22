c95926f575f7b814f49efcda9459296863851b85
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-05-22 17:03:47 +0900

    style: 주점 관리 페이지 틀 수정

--- src/app/(route)/admin/pub-management/page.tsx
+++ src/app/(route)/admin/pub-management/page.tsx
@@ -1,7 +1,7 @@
 export default function PubManagement() {
   return (
-    <div className="bg- flex min-h-screen flex-col items-center justify-center bg-deep-cove">
-      <h1>주점 관리</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-deep-cove">
+      주점 관리 페이지
     </div>
   );
 }
