dc0de5b530c8fca4246130fd38637b1b96bb4def
jodan <wdd789q@naver.com>
2024-05-23 17:48:01 +0900

    feat: 스크롤바 숨기는 tailwindcss 라이브러리 설치 & 웨이팅 나타내는 인원수 설정 View구현

--- src/app/(route)/landing/[id]/page.tsx
+++ src/app/(route)/landing/[id]/page.tsx
@@ -1,13 +1,13 @@
 // src/app/(route)/landing/[id]/page.tsx
-"use client"; // Add the "use client" pragma comment
+"use client";
 import React from 'react';
-import { useRouter, usePathname } from 'next/navigation';
-import storeData from '../data.json'; // Adjust the path to your JSON file
+import { usePathname } from 'next/navigation';
+import storeData from '../data.json';
+import WaitingTeams from './WaitingTeams';
 
 const StoreDetailPage: React.FC = () => {
-  const router = useRouter();
-  const pathname = usePathname(); // Use usePathname to get the current URL pathname
-  const id = pathname.split('/').pop(); // Extract the id from the pathname
+  const pathname = usePathname();
+  const id = pathname.split('/').pop();
 
   const store = storeData.stores.find((store) => store.id === Number(id));
 
@@ -16,9 +16,10 @@ const StoreDetailPage: React.FC = () => {
   }
 
   return (
-    <div className="h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <div className="w-9/12 h-full max-w-2xl bg-white rounded-lg shadow-md p-6 border border-red-500">
-        <div className="w-full flex justify-between mb-10" >
+    <div className="h-screen bg-gray-100 flex flex-col items-center justify-center ">
+      <div className="w-9/12 h-full max-w-2xl bg-white rounded-lg shadow-md p-6 border border-red-500 overflow-auto scrollbar-hide">
+        <div className="w-full flex justify-between mb-10">
+          {/* 뒤로가기 & 북마크 */}
           <div>
             <img src='/images/return.png' alt='뒤로가기' className="w-7 h-7" />
           </div>
@@ -30,7 +31,8 @@ const StoreDetailPage: React.FC = () => {
             )}
           </div>
         </div>
-        <div style={{ border: '1px solid red' }}>
+        {/* 가게 정보 텍스트 */}
+        <div>
           <div>
             {store.people === "많음" &&
               <div className="flex justify-left items-center">
@@ -53,13 +55,17 @@ const StoreDetailPage: React.FC = () => {
           </div>
           <h1 className="text-2xl font-bold mb-4">{store.department}</h1>
           <p>{store.introduction}</p>
-          <p className="w-12/12 bg-white flex justify-start mb-[0.5%] text-[10px] items-start text-left">{store.studentIDRequired ? '학생증 필요' : '학생증 불필요'}</p>
-          <p className="w-12/12 bg-white flex justify-start mb-[0.5%] text-[10px] items-start text-left">{store.menu[0].name}</p>
-          <div className="text-lg font-bold" style={{ borderBottom: '2px solid black', width: 'min-content' }}>Instagram</div>
+          <p className="w-full bg-white flex justify-start mb-[0.5%] text-[10px] items-start text-left">{store.studentIDRequired ? '학생증 필요' : '학생증 불필요'}</p>
+          <p className="w-full bg-white flex justify-start mb-[0.5%] text-[10px] items-start text-left">{store.menu[0].name}</p>
+          <div className="text-lg font-bold border-b-2 border-black w-min">Instagram</div>
         </div>
-        <div className="border-b border-gray-300 flex flex-row mt-5 h-1/6 ">
+        {/* 메뉴 & 전화 카테고리 */}
+        <div className="border-b border-gray-300 flex flex-row mt-5 h-1/6">
           <div className="w-1/2 text-lg flex justify-center items-center border-r border-gray-300">전화</div>
           <div className="w-1/2 text-lg flex justify-center items-center">메뉴</div>
+        </div>
+        <div className="mt-4">
+          <WaitingTeams />
         </div>
       </div>
     </div>
