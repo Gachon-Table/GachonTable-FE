33449c8c5f69e42892e1120669145f844c930737
jodan <wdd789q@naver.com>
2024-05-23 12:52:53 +0900

    feat: 잔화&메뉴 메뉴판 View 구현

--- src/app/(route)/landing/[id]/page.tsx
+++ src/app/(route)/landing/[id]/page.tsx
@@ -18,7 +18,7 @@ const StoreDetailPage: React.FC = () => {
   return (
     <div className="h-screen bg-gray-100 flex flex-col items-center justify-center">
       <div className="w-9/12 h-full max-w-2xl bg-white rounded-lg shadow-md p-6 border border-red-500">
-        <div style={{ width: '100%', border: '1px solid red', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+        <div className="w-full flex justify-between mb-10" >
           <div>
             <img src='/images/return.png' alt='뒤로가기' className="w-7 h-7" />
           </div>
@@ -30,24 +30,24 @@ const StoreDetailPage: React.FC = () => {
             )}
           </div>
         </div>
-        <div>
+        <div style={{ border: '1px solid red' }}>
           <div>
             {store.people === "많음" &&
-              <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'left', alignItems: 'center' }}>
+              <div className="flex justify-left items-center">
                 <img src="/images/manypeople.png" alt="많은 사람" className="w-4 h-4 mr-1" />
-                <p style={{ color: '#EF2323', fontWeight: 'bold' }}>많음</p>
+                <p className="text-red-600 font-bold">많음</p>
               </div>
             }
             {store.people === "보통" &&
-              <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'left', alignItems: 'center' }}>
+              <div className="flex justify-left items-center">
                 <img src="/images/normalpeople.png" alt="보통 사람" className="w-4 h-4 mr-1" />
-                <p style={{ color: '#F4DE18', fontWeight: 'bold' }}>보통</p>
+                <p className="text-yellow-500 font-bold">보통</p>
               </div>
             }
             {store.people === "적음" &&
-              <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'left', alignItems: 'center' }}>
+              <div className="flex justify-left items-center">
                 <img src="/images/nonepeople.png" alt="적은 사람" className="w-4 h-4 mr-1" />
-                <p style={{ color: '#06C33B', fontWeight: 'bold' }}>적음</p>
+                <p className="text-green-500 font-bold">적음</p>
               </div>
             }
           </div>
@@ -55,9 +55,12 @@ const StoreDetailPage: React.FC = () => {
           <p>{store.introduction}</p>
           <p className="w-12/12 bg-white flex justify-start mb-[0.5%] text-[10px] items-start text-left">{store.studentIDRequired ? '학생증 필요' : '학생증 불필요'}</p>
           <p className="w-12/12 bg-white flex justify-start mb-[0.5%] text-[10px] items-start text-left">{store.menu[0].name}</p>
-          <div style={{fontSize: '16px', fontWeight: 'bold',width:'min-content' , borderBottom: '2px solid black'}}>Instagram</div>
+          <div className="text-lg font-bold" style={{ borderBottom: '2px solid black', width: 'min-content' }}>Instagram</div>
         </div>
-
+        <div className="border-b border-gray-300 flex flex-row mt-5 h-1/6 ">
+          <div className="w-1/2 text-lg flex justify-center items-center border-r border-gray-300">전화</div>
+          <div className="w-1/2 text-lg flex justify-center items-center">메뉴</div>
+        </div>
       </div>
     </div>
   );
