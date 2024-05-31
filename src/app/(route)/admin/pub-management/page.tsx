5f454f5403be0ffae119aae2a300088cd302a5de
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-05-31 15:53:33 +0900

    style: 이미지 업로드 박스 적용

--- src/app/(route)/admin/pub-management/page.tsx
+++ src/app/(route)/admin/pub-management/page.tsx
@@ -1,10 +1,24 @@
+'use client';
+
+import { useState } from 'react';
 import Dropdown from '../_components/Dropdown';
+import ImageInputBox from '../_components/ImageInputBox';
 
 export default function PubManagement() {
+  const [contentImages, setContentImages] = useState<File[]>([]);
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-deep-cove">
-      <div className="w-11/12 max-w-screen-xl pb-11">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-deep-cove pt-8">
+      <div className="w-11/12 max-w-screen-xl pb-5">
         <Dropdown />
+      </div>
+      <div className="h-[480px] w-11/12 max-w-screen-xl overflow-y-auto rounded-lg bg-white p-5 shadow-md">
+        <ImageInputBox
+          contentImagesState={{
+            contentImages,
+            setContentImages,
+          }}
+        />
       </div>
     </div>
   );
