128c26b7bb770f9064fd456b056664af9bfe0350
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-06-03 10:09:15 +0900

    feat: 모듈 설치

---
+++ src/app/(route)/admin/field-lineup/page.tsx
@@ -0,0 +1,12 @@
+export default function FieldLineUp() {
+    return (
+        <div className="flex flex-row">
+            <div className="flex flex-col bg-deep-cove">
+                <img src="/images/logo3.png" />
+            </div>
+            <div className="flex flex-col bg-white">
+                <div>웨이팅 알림을 받을 수 있는 전화번호를 입력해주세요.</div>
+            </div>
+        </div>
+    )
+}
