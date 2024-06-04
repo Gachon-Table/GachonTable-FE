c8e7b3f279bd86b98581980b73f9dcbfe45f0cf7
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-06-04 18:25:10 +0900

    style: 현장 줄서기 정보 컴포넌트 구현 및 적용

--- src/app/(route)/admin/field-lineup/page.tsx
+++ src/app/(route)/admin/field-lineup/page.tsx
@@ -1,27 +1,18 @@
 import NumberKeypad from '../_components/NumberKeypad';
+import Information from '../_components/Information';
 
 export default function FieldLineUp() {
+  const data = {
+    pubName: '경제학과 이코노미더머니',
+    queueing: 2,
+  };
+
   return (
     <div className="flex h-[768px] w-[1024px] flex-row items-center justify-center">
-      {/* Left Section */}
-      <div className="flex h-full w-1/2 flex-col items-center justify-center bg-deep-cove text-white">
-        <div className="flex flex-col items-center">
-          <img src="/images/logo3.png" alt="Logo" width={100} height={100} />
-          <p className="mt-4 text-center text-lg">
-            휴대폰 번호를 입력하시면 <br />
-            카카오톡으로 웨이팅 현황을 <br />
-            알림으로 알려드려요
-          </p>
-          <p className="mt-8 text-center text-2xl font-bold">
-            경제학과 이코노미더머니
-          </p>
-          <p className="mt-4 text-center text-xl">
-            현재 대기 <span className="text-4xl text-red-500">5</span>팀
-          </p>
-        </div>
+      <div className="flex h-full w-1/2 flex-col items-center bg-deep-cove text-white">
+        <Information pubName={data.pubName} queueing={data.queueing} />
       </div>
 
-      {/* Right Section */}
       <div className="flex h-full w-1/2 flex-col bg-white">
         <NumberKeypad />
       </div>
