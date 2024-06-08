854f914909b532d1ed96878c5fddac3ba0de153e
ggome1 <bookincar@naver.com>
2024-06-08 16:35:16 +0900

    fix: nav ui 수정

--- src/app/(route)/_components/nav/Navigation.tsx
+++ src/app/(route)/_components/nav/Navigation.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Navigation: React.FC = () => {
     return (
-        <nav className="w-full fixed bottom-0 left-0 right-0 ">
-            <nav className="mx-auto w-full max-w-30rem flex justify-evenly border-none ">
+        <nav className="mobile:max-w-[470px] mobile:mx-auto border-none bg-white fixed bottom-0 left-0 right-0 ">
+            <nav className="mx-auto w-full max-w-30rem flex justify-evenly ">
                 <button className="p-2.5 bg-transparent border-none text-lg text-gray-800 transition-all ease-in-out duration-300 w-1/3 hover:text-blue-500 flex justify-center items-center">
                     <img src="/images/Home.png" alt="홈" className="w-8 h-8" />
                 </button>
--- src/app/(route)/mypage/_components/afterProfile/page.tsx
+++ src/app/(route)/mypage/_components/afterProfile/page.tsx
@@ -4,11 +4,11 @@ import React from 'react';
 const AfterProfile = () => {
   const { data: session } = useSession();
   return (
-    <div className="mr-[8rem] mt-[8rem] flex w-[100%] justify-evenly gap-10">
-      <div className="flex h-full">
+    <div className="pl-[3rem] mt-[8rem] flex w-[100%] gap-[5rem]">
+      <div className="flex h-[6rem]">
         <img src="/images/profile.png" />
       </div>
-      <div className="mt-2 flex w-[10rem] flex-col items-center justify-center text-[30px]">
+      <div className="flex items-center text-[30px]">
         <div>{session?.user?.name}님</div>
       </div>
     </div>
