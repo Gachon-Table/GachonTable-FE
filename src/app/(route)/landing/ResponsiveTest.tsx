eb6163dda2f4a119181d4d428e45262bb446af0a
jodan <wdd789q@naver.com>
2024-07-06 20:26:48 +0900

    feat : 주점목록 불러오기 API 연동 재수정 및 검색 기능 구현

--- src/app/(route)/landing/ResponsiveTest.tsx
+++ src/app/(route)/landing/ResponsiveTest.tsx
@@ -1,29 +1,31 @@
-"use client"
+"use client";
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Link from 'next/link'; // Import Link from next/link
 
 interface Store {
-  id: number;
-  department: string;
+  pubId: number;
+  pubName: string;
   people: string;
-  introduction: string;
-  studentIDRequired: boolean;
-  menu: { name: string }[];
+  oneLiner: string;
+  studentCard: boolean;
+  menu: string;
   bookmark: boolean;
 }
 
-const ResponsiveTest: React.FC = () => {
+interface ResponsiveTestProps {
+  searchTerm: string;
+}
+
+const ResponsiveTest: React.FC<ResponsiveTestProps> = ({ searchTerm }) => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true); // 로그인 상태 관리
   const [showLoginPopup, setShowLoginPopup] = useState<boolean>(false); // 팝업 상태 관리
   const [stores, setStores] = useState<Store[]>([]); // 가게 정보 저장
   const [loading, setLoading] = useState<boolean>(true); // 로딩 상태 관리
   const [error, setError] = useState<string | null>(null); // 에러 상태 관리
 
-  //주점목록 API
+  // 주점목록 API
   useEffect(() => {
-    // API 호출하여 데이터 가져오기
-    axios.get('http://ec2-3-34-185-126.ap-northeast-2.compute.amazonaws.com:8080/pubs')
+    axios.get('http://ec2-3-34-185-126.ap-northeast-2.compute.amazonaws.com:8080/pub/all')
       .then(response => {
         console.log(response.data); // 받아온 데이터 콘솔에 출력
         setStores(response.data); // 받아온 데이터 상태에 저장
@@ -37,7 +39,7 @@ const ResponsiveTest: React.FC = () => {
       });
   }, []);
 
-  const handleStoreClick = (e: React.MouseEvent<HTMLAnchorElement>, id: number) => {
+  const handleStoreClick = (e: React.MouseEvent<HTMLAnchorElement>, pubId: number) => {
     if (!isLoggedIn) {
       e.preventDefault(); // 페이지 이동 막기
       setShowLoginPopup(true); // 팝업 표시
@@ -47,6 +49,13 @@ const ResponsiveTest: React.FC = () => {
   const closePopup = () => {
     setShowLoginPopup(false);
   };
+
+  // 검색어에 따라 데이터를 필터링
+  const filteredStores = stores.filter(store =>
+    store.pubName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    store.oneLiner.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    store.menu.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <>
@@ -58,16 +67,16 @@ const ResponsiveTest: React.FC = () => {
         <div className="flex justify-center items-center h-screen">
           <p>{error}</p>
         </div>
-      ) : stores.length === 0 ? (
+      ) : filteredStores.length === 0 ? (
         <div className="flex justify-center items-center h-screen">
           <p>현재 주점이 없습니다</p>
         </div>
       ) : (
-        stores.map((store: Store) => (
+        filteredStores.map((store: Store) => (
           <a
-            href={`/landing/${store.id}`}
-            key={store.id}
-            onClick={(e) => handleStoreClick(e, store.id)}
+            href={`/landing/${store.pubId}`}
+            key={store.pubId}
+            onClick={(e) => handleStoreClick(e, store.pubId)}
             className="w-full h-1/10 bg-white flex flex-row"
           >
             <div className="w-full h-1/10 bg-white flex flex-row">
@@ -75,16 +84,16 @@ const ResponsiveTest: React.FC = () => {
               <div className="w-4/5 bg-white flex flex-row justify-between">
                 <div>
                   <div className="w-11/12 bg-white flex items-center text-center justify-start text-[20px]">
-                    <div className="font-bold text-lg">{store.department}</div>
+                    <div className="font-bold text-lg">{store.pubName}</div>
                     <div className="ml-2">
                       {store.people === "많음" && <img src="/images/manypeople.png" alt="많은 사람" className="w-4 h-4 mr-1" />}
                       {store.people === "보통" && <img src="/images/normalpeople.png" alt="보통 사람" className="w-4 h-4 mr-1" />}
                       {store.people === "적음" && <img src="/images/nonepeople.png" alt="적은 사람" className="w-4 h-4 mr-1" />}
                     </div>
                   </div>
-                  <div className="w-80 bg-white flex justify-start mb-[1%] text-base items-start text-left">{store.introduction}</div>
-                  <div className="w-12/12 bg-white flex justify-start mb-[0.5%] text-[10px] items-start text-left">{store.studentIDRequired ? '학생증 필요' : '학생증 불필요'}</div>
-                  <div className="w-12/12 bg-white flex justify-start mb-[0.5%] text-[10px] items-start text-left">{store.menu[0].name}</div> {/* 첫 번째 메뉴 이름만 표시 */}
+                  <div className="w-80 bg-white flex justify-start mb-[1%] text-base items-start text-left">{store.oneLiner}</div>
+                  <div className="w-12/12 bg-white flex justify-start mb-[0.5%] text-[10px] items-start text-left">{store.studentCard ? '학생증 필요' : '학생증 불필요'}</div>
+                  <div className="w-12/12 bg-white flex justify-start mb-[0.5%] text-[10px] items-start text-left">{store.menu}</div>
                 </div>
                 <div>
                   {store.bookmark ? (
--- src/app/(route)/landing/page.tsx
+++ src/app/(route)/landing/page.tsx
@@ -1,12 +1,14 @@
-// src/app/(route)/landing/page.tsx
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
 import ResponsiveTest from './ResponsiveTest';
 import Navigation from '../_components/nav/Navigation';
 
 const LandingPage: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <header className="w-full max-w-2xl h-full bg-white rounded-lg shadow-md text-center mt-6">
+      <header className="fixed top-0 w-full max-w-[30rem] bg-white rounded-lg shadow-md text-center mt-1">
         <div className="w-full h-[7.5vh] bg-white rounded-lg flex items-center justify-center">
           <div className="w-1/5 bg-white flex items-center justify-center rounded-lg">
             <div className="w-11 h-11 bg-gray-300 rounded-full"></div>
@@ -16,6 +18,8 @@ const LandingPage: React.FC = () => {
               type="text"
               className="w-4/5 h-10 bg-gray-200 rounded-full border-none px-4"
               placeholder="Search..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
             />
           </div>
         </div>
@@ -27,8 +31,8 @@ const LandingPage: React.FC = () => {
           </div>
         </div>
       </header>
-      <main className="flex flex-col items-center w-full max-w-2xl mt-6 gap-6 h-full">
-        <ResponsiveTest />
+      <main className="flex flex-col items-center w-full max-w-2xl mt-[20vh] gap-6 h-full overflow-y-auto">
+        <ResponsiveTest searchTerm={searchTerm} />
       </main>
       <Navigation />
     </div>
