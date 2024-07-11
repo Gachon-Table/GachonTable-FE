ed6cc4c1facaad4241f609f6bc1c4710bfad55d9
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-07-12 00:10:35 +0900

    style: alert modal UI 수정

--- src/app/(route)/admin/_components/AlertModal.tsx
+++ src/app/(route)/admin/_components/AlertModal.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 
 interface AlertModalProps {
   message: string;
+  button: string;
   onCancel: () => void;
 }
 
-const AlertModal: React.FC<AlertModalProps> = ({ message, onCancel }) => {
+const AlertModal: React.FC<AlertModalProps> = ({
+  message,
+  button,
+  onCancel,
+}) => {
   return (
     <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center">
       <div className="flex flex-col items-center justify-center rounded-lg bg-gray-200 p-6 shadow-lg">
         <div className="text-lg font-medium">{message}하시겠습니까?</div>
         <div className="mt-2 text-sm font-light">
-          맞는지 확인 후 호출 버튼을 눌러주세요.
+          맞는지 확인 후 {button} 버튼을 눌러주세요.
         </div>
         <div className="mt-4 flex items-center justify-center space-x-4">
           <button
@@ -20,7 +25,7 @@ const AlertModal: React.FC<AlertModalProps> = ({ message, onCancel }) => {
           >
             취소
           </button>
-          <button className="rounded-lg bg-blue-500 px-5 py-3 text-sm font-light text-white shadow-md hover:bg-blue-600">
+          <button className="bg-point-red rounded-lg px-5 py-3 text-sm font-light text-white shadow-md">
             {message.substring(message.length - 2)}
           </button>
         </div>
