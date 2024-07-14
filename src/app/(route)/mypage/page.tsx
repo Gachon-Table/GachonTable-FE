583d6dd61092cb7d9c7537dfe0afbbb671c8d33e
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-07-15 01:05:20 +0900

    fix: 마이페이지 오류 부분 주석처리

--- src/app/(route)/mypage/page.tsx
+++ src/app/(route)/mypage/page.tsx
@@ -1,25 +1,25 @@
 'use client';
 
-import { useState } from 'react';
-import Waited from './_components/waited/page';
-import Waiting from './_components/waiting/page';
-import BeforeProfile from './_components/beforeProfile/page';
-import Tab from './_components/tab/page';
-import Navigation from '../_components/nav/Navigation';
-import { useSession } from 'next-auth/react';
-import AfterProfile from './_components/afterProfile/page';
+import React, { useState } from 'react';
+// import Waited from './_components/waited/page';
+// import Waiting from './_components/waiting/page';
+// import BeforeProfile from './_components/beforeProfile/page';
+// import Tab from './_components/tab/page';
+// import Navigation from '../_components/nav/Navigation';
+// import { useSession } from 'next-auth/react';
+// import AfterProfile from './_components/afterProfile/page';
 
 const Mypage = () => {
-  const [curTab, setCurTab] = useState('ing');
-  const { data: session } = useSession();
+  // const [curTab, setCurTab] = useState('ing');
+  // const { data: session } = useSession();
 
   return (
     <div className="flex flex-col justify-center">
-      {session ? <AfterProfile /> : <BeforeProfile />}
+      {/* {session ? <AfterProfile /> : <BeforeProfile />}
       <Tab curTab={curTab} setFunc={setCurTab} />
       {curTab === 'ing' && <Waiting />}
       {curTab === 'ed' && <Waited />}
-      <Navigation />
+      <Navigation /> */}
     </div>
   );
 };
--- src/app/api/mypage/waitingApi/waitedApi/page.ts
+++ src/app/api/mypage/waitingApi/waitedApi/page.ts
@@ -1,14 +1,14 @@
-"use client"
-import getRequest from '@/app/service/getRequest';
-import { useSession } from 'next-auth/react';
-import { NextResponse } from 'next/server';
+// import getRequest from '@/app/service/getRequest';
+// import { useSession } from 'next-auth/react';
+// import { NextResponse } from 'next/server';
 
 const WaitedApi = async() => {
-  const { data: session } = useSession();
-  const accessToken = session?.accessToken;
-  const url = 'url';
+  // const { data: session } = useSession();
+  // const accessToken = session?.accessToken;
+  // const url = 'url';
 
-  return NextResponse.json(await getRequest(url, accessToken));
+  // return NextResponse.json(await getRequest(url, accessToken));
+  return;
 };
 
 export default WaitedApi;
