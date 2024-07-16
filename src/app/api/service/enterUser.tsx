0e283a6aaafab94cc78bf15d94aec379ab05969e
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-07-16 17:14:09 +0900

    fix: 유저 호출, 입장 완료 및 주점 정보 업데이트 로직 수정

--- src/app/api/service/enterUser.tsx
+++ src/app/api/service/enterUser.tsx
@@ -6,8 +6,14 @@ interface userProps {
 
 export const enterUser = async (credentials: userProps) => {
   try {
-    await adminAxios.patch('/enter', credentials);
-    console.log('입장에 성공하였습니다.');
+    const response = await adminAxios.patch('/enter', credentials);
+    if (response.status === 200) {
+      console.log('입장에 성공하였습니다.');
+      alert('입장 성공');
+    } else {
+      console.log('입장에 실패하였습니다.');
+      alert('입장 실패');
+    }
   } catch (error) {
     console.error('입장 실패: ', error);
     throw error;
--- src/app/api/service/savePubInfo.tsx
+++ src/app/api/service/savePubInfo.tsx
@@ -16,8 +16,11 @@ interface PubProps {
 export const savePubInfo = async (credentials: PubProps) => {
   try {
     const response = await pubAxios.patch('/manage', credentials);
-
-    return response.data;
+    if (response.status === 200) {
+      console.log('정보 업데이트 성공');
+      alert('저장되었습니다.');
+      return response.data;
+    }
   } catch (error) {
     console.log('정보 업데이트 실패: ', error);
   }
