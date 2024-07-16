37244dfdb01e5721717aaba6ecd80e5e694afa7c
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-07-16 16:28:55 +0900

    feat: 대기 마감 기능 구현

--- src/app/api/service/handleStatus.tsx
+++ src/app/api/service/handleStatus.tsx
@@ -1,22 +1,22 @@
-// import adminAxios from '../axios/adminAxios';
-// import { getPubInfo } from './getPubInfo';
+import adminAxios from '../axios/adminAxios';
+import { getPubInfo } from './getPubInfo';
 
-// export const handleStatus = async () => {
-//   try {
-//     const data = await getPubInfo();
-//     const newStatus = !data.pubStatus;
-//     const response = await adminAxios.patch('/status', {
-//       openStatus: newStatus,
-//     });
+export const handleStatus = async () => {
+  try {
+    const data = await getPubInfo();
+    const newStatus = !data.pub.openStatus;
+    const response = await adminAxios.patch('/status', {
+      openStatus: newStatus,
+    });
 
-//     if (response.status === 200) {
-//       console.log('상태 변경 성공');
-//       return response.data;
-//     } else {
-//       throw new Error('상태 변경 실패');
-//     }
-//   } catch (error) {
-//     console.error('대기 상태 변경 실패: ', error);
-//     throw error;
-//   }
-// };
+    if (response.status === 200) {
+      console.log('상태 변경 성공');
+      return response.data;
+    } else {
+      throw new Error('상태 변경 실패');
+    }
+  } catch (error) {
+    console.error('대기 상태 변경 실패: ', error);
+    throw error;
+  }
+};
