c6d7bee1b723617656bf2b40f9c482dbbf08f690
ggome1 <bookincar@naver.com>
2024-07-18 14:10:09 +0900

    fix: footer ui

--- src/app/(route)/_components/footer/page.tsx
+++ src/app/(route)/_components/footer/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 const Footer = () => {
   return (
-    <div className="flex h-40 flex-col items-center justify-center p-4">
-      <div className="mb-2 text-center">회사명: 참숯한우천국</div>
-      <div className="mb-2 text-center">대표자: 김선창</div>
-      <div className="mb-2 text-center">사업자 등록 번호: 488-28-01237</div>
-      <div className="text-center">
-        주소: 경기도 부천시 원미구 상기로 21번길 26-15 1층
+    <div className="flex h-40 flex-col justify-center px-4 py-2 border-t-2 border-b-2 bg-gray-100 text-gray-400">
+      <div className='font-bold'>정보</div>
+      <div>회사명: 참숯한우천국</div>
+      <div>대표자: 김선창</div>
+      <div>사업자 등록 번호: 488-28-01237</div>
+      <div>
+        주소: 경기도 부천시 원미구 상이로21번길 26-15, 1층
       </div>
     </div>
   );
