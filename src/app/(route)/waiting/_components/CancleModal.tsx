707977551a8f2f3ff9040694785e3862ddf8add4
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-08-12 04:47:53 +0900

    style: CancleModal UI 수정

--- src/app/(route)/waiting/_components/CancleModal.tsx
+++ src/app/(route)/waiting/_components/CancleModal.tsx
@@ -21,7 +21,7 @@ const CancelModal = ({ waitingId, setModal }: CancelModalProps) => {
     setModal(false);
   };
   return (
-    <div className="absolute my-auto h-full w-full max-w-[500px]">
+    <div className="max-w-[500px absolute my-auto h-full w-full">
       <div
         className="flex h-full w-full items-center justify-center"
         onClick={() => setModal(false)}
@@ -30,21 +30,21 @@ const CancelModal = ({ waitingId, setModal }: CancelModalProps) => {
           onClick={(e) => {
             e.stopPropagation();
           }}
-          className="flex h-[30vh] w-[80%] flex-col items-center justify-center gap-2 rounded-[0.5rem] border-2 border-gray-300 bg-white py-4"
+          className="flex w-4/6 flex-col items-center justify-center gap-2 rounded-2xl bg-[#FEFEFF] py-4 shadow-md shadow-gray-300/50"
         >
-          <div className="text-xl font-bold">줄서기 취소 하시겠습니까?</div>
-          <div className="text-gray-400">
+          <div className="text-md font-medium">줄서기 취소 하시겠습니까?</div>
+          <div className="text-sm font-light text-[#52575C]">
             맞는지 확인 후 확인 버튼을 눌러 주세요
           </div>
-          <div className="mt-[2rem] flex w-full justify-evenly">
+          <div className="mt-3 flex w-full justify-evenly">
             <div
-              className="cursor-pointer rounded-[1rem] bg-[#f5f5f5] px-6 py-4"
+              className="cursor-pointer rounded-lg bg-[#f5f5f5] px-6 py-4"
               onClick={() => setModal(false)}
             >
               취소
             </div>
             <div
-              className="cursor-pointer rounded-[1rem] bg-[#e87567] px-6 py-4 text-white"
+              className="cursor-pointer rounded-lg bg-[#e87567] px-6 py-4 text-white"
               onClick={handleCancel}
             >
               확인
