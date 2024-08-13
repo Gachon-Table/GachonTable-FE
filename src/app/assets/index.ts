b0b569e7a4a857a5f5e095966c1726c594d68c53
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-08-14 02:19:43 +0900

    chore: 이미지 svg 변환

--- src/app/assets/index.ts
+++ src/app/assets/index.ts
@@ -1,4 +1,7 @@
-// export { default as BackIcon } from './svgs/back-button.svg';
-export { default as DeleteRed } from './svgs/delete-red.svg';
+export { default as DeleteRed } from './svgs/delete-red.svg'
 export { default as LogoCircle } from './svgs/logo-circle.svg'
 export { default as AlertIcon } from './svgs/alert-icon.svg'
+export { default as KakaoLogin } from './svgs/kakao-login.svg'
+export { default as KakaoLoginRound } from './svgs/kakao-login-round.svg'
+export { default as Logo } from './svgs/logo.svg'
+export { default as LeftArrow } from './svgs/left-arrow.svg'
