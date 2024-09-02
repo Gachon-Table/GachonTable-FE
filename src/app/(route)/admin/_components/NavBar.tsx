f7251400e0cb40b0f96a42316e1410d3ca8e8a79
chasyuss <cotn963@gmail.com>
2024-09-03 01:46:50 +0900

    style: navbar 관련 ui 수정

--- src/app/(route)/admin/_components/NavBar.tsx
+++ src/app/(route)/admin/_components/NavBar.tsx
@@ -80,75 +80,49 @@ export const Navbar = () => {
   }, []);
 
   return (
-    <nav>
-      <div className="flex flex-row items-center">
-        <div className="w-20 flex-grow border-t border-[#C2C2C2]" />
-        <div className="flex h-16 flex-shrink-0">
-          <div className="flex items-center">
-            <div className="flex flex-row space-x-1">
-              <Link
-                href="/admin/waiting-management"
-                className={`${isActive('/admin/waiting-management') ? 'border-main-blue text-[#434343]' : 'border-transparent text-[#969595] hover:border-main-blue hover:text-[#434343]'} flex items-center border-b-2 px-2 py-2 text-sm font-medium`}
-              >
-                대기 인원
-              </Link>
-              <Link
-                href="/admin/pub-management"
-                className={`${isActive('/admin/pub-management') ? 'border-main-blue text-[#434343]' : 'border-transparent text-[#969595] hover:border-main-blue hover:text-[#434343]'} flex items-center border-b-2 px-2 py-2 text-sm font-medium`}
-              >
-                주점 메뉴
-              </Link>
-              <Link
-                href="/admin/onsite-lineup"
-                className={`${isActive('/admin/onsite-lineup') ? 'border-main-blue text-[#434343]' : 'border-transparent text-[#969595] hover:border-main-blue hover:text-[#434343]'} flex items-center border-b-2 px-2 py-2 text-sm font-medium`}
-              >
-                현장줄서기
-              </Link>
-              <div className="relative">
-                <button
-                  onClick={toggleDropdown}
-                  className={`${isActive('/설정') ? 'border-main-blue text-[#434343]' : 'border-transparent text-[#969595] hover:border-main-blue hover:text-[#434343]'} flex items-center border-b-2 px-4 py-2 text-sm font-medium`}
-                >
-                  설 정
-                </button>
-                {isDropdownOpen && (
-                  <div className="absolute right-0 mt-2 w-16 rounded-lg bg-white shadow-lg ">
-                    <div
-                      className="py-1"
-                      role="menu"
-                      aria-orientation="vertical"
-                      aria-labelledby="options-menu"
-                    >
-                      {openStatus !== null && (
-                        <a
-                          onClick={handleStatusClick}
-                          className="block cursor-pointer py-2 pl-2.5 text-xs font-medium text-[#969595] hover:text-[#434343]"
-                          role="menuitem"
-                        >
-                          {openStatus ? '대기마감' : '대기오픈'}
-                        </a>
-                      )}
-                      <a
-                        onClick={handleLogoutClick}
-                        className="block cursor-pointer py-2 pl-2.5 text-xs font-medium text-[#969595] hover:text-[#434343]"
-                        role="menuitem"
-                      >
-                        로그아웃
-                      </a>
-                    </div>
-                  </div>
-                )}
-              </div>
-            </div>
+    <nav className="ml-8 w-full">
+      <div className="mt-6">
+        <div className="mb-6 flex h-16 items-center">
+          <div className="mr-8 flex w-full border-b-2 border-gray-200">
+            <Link
+              href="/admin/pub-management"
+              className={`flex flex-1 items-center justify-center px-2 py-2 text-base font-semibold ${
+                isActive('/admin/pub-management')
+                  ? 'border-gy-900 text-gy-900 -mb-0.5 border-b-2'
+                  : 'text-gy-400 hover:text-gy-400'
+              }`}
+            >
+              메뉴 관리
+            </Link>
+
+            <Link
+              href="/admin/waiting-management"
+              className={`flex flex-1 items-center justify-center px-2 py-2 text-base font-semibold ${
+                isActive('/admin/waiting-management')
+                  ? 'border-gy-900 text-gy-900 -mb-0.5 border-b-2'
+                  : 'text-gy-400 hover:text-gy-400'
+              }`}
+            >
+              고객 관리
+            </Link>
+
+            <Link
+              href="/admin/setting"
+              className={`flex flex-1 items-center justify-center px-2 py-2 text-base font-semibold ${
+                isActive('/admin/setting')
+                  ? 'border-gy-900 text-gy-900 -mb-0.5 border-b-2'
+                  : 'text-gy-400 hover:text-gy-400'
+              }`}
+            >
+              설정
+            </Link>
           </div>
         </div>
-        <div className="w-20 flex-grow border-t border-[#C2C2C2]" />
       </div>
 
       {showStatusModal && (
         <AlertModal
           message={openStatus ? '대기 마감' : '대기 오픈'}
-          button={openStatus ? '마감' : '오픈'}
           onCancel={() => setShowStatusModal(false)}
           onConfirm={confirmStatus}
         />
@@ -157,7 +131,6 @@ export const Navbar = () => {
       {showLogoutModal && (
         <AlertModal
           message="로그아웃"
-          button="로그아웃"
           onCancel={() => setShowLogoutModal(false)}
           onConfirm={confirmLogout}
         />
