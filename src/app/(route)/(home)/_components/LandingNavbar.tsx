fa52915ca6c4496e4c572818f8121a0a5ab1b3b1
chasyuss <cotn963@gmail.com>
2024-09-05 21:31:52 +0900

    refactor: Landing page 컴포넌트 분리 및 ui 수정

---
+++ src/app/(route)/(home)/_components/LandingNavbar.tsx
@@ -0,0 +1,89 @@
+'use client';
+import React, { useState } from 'react';
+
+const LandingNavbar: React.FC<{ onFilterChange: (filters: any) => void }> = ({
+  onFilterChange,
+}) => {
+  const [filterStudentCard, setFilterStudentCard] = useState<boolean | null>(
+    null,
+  );
+  const [sortByCongestion, setSortByCongestion] = useState(false);
+  const [sortByLowCongestion, setSortByLowCongestion] = useState(false);
+
+  const handleStudentCardFilter = () => {
+    const newValue = filterStudentCard === false ? null : false;
+    setFilterStudentCard(newValue);
+    setSortByCongestion(false);
+    setSortByLowCongestion(false);
+    onFilterChange({
+      filterStudentCard: newValue,
+      sortByCongestion: false,
+      sortByLowCongestion: false,
+    });
+  };
+
+  const handleSortByCongestion = () => {
+    setSortByCongestion((prev) => !prev);
+    setSortByLowCongestion(false);
+    setFilterStudentCard(null);
+    onFilterChange({
+      sortByCongestion: !sortByCongestion,
+      sortByLowCongestion: false,
+      filterStudentCard: null,
+    });
+  };
+
+  const handleSortByLowCongestion = () => {
+    setSortByLowCongestion((prev) => !prev);
+    setSortByCongestion(false);
+    setFilterStudentCard(null);
+    onFilterChange({
+      sortByLowCongestion: !sortByLowCongestion,
+      sortByCongestion: false,
+      filterStudentCard: null,
+    });
+  };
+
+  return (
+    <nav className="w-full">
+      <div className="mb-6 flex items-center bg-wt">
+        <div className="flex w-full border-b-2 border-gray-200">
+          <div
+            onClick={handleSortByCongestion}
+            className={`flex flex-1 cursor-pointer items-center justify-center px-2 py-2 transition duration-300 ease-in-out font-b1-normal-semibold ${
+              sortByCongestion
+                ? '-mb-0.5 border-b-2 border-gy-900 text-gy-900'
+                : 'text-gy-400 hover:border-gy-900 hover:text-gy-900'
+            }`}
+          >
+            대기 적은 순
+          </div>
+
+          <div
+            onClick={handleSortByLowCongestion}
+            className={`flex flex-1 cursor-pointer items-center justify-center px-2 py-2 transition duration-300 ease-in-out font-b1-normal-semibold ${
+              sortByLowCongestion
+                ? '-mb-0.5 border-b-2 border-gy-900 text-gy-900'
+                : 'text-gy-400 hover:border-gy-900 hover:text-gy-900'
+            }`}
+          >
+            대기 많은 순
+          </div>
+
+          <div
+            onClick={handleStudentCardFilter}
+            className={`flex flex-1 cursor-pointer items-center justify-center px-2 py-2 transition duration-300 ease-in-out font-b1-normal-semibold ${
+              filterStudentCard === false
+                ? '-mb-0.5 border-b-2 border-gy-900 text-gy-900'
+                : 'text-gy-400 hover:border-gy-900 hover:text-gy-900'
+            }`}
+          >
+            학생증 불필요
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default LandingNavbar;
