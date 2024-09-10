0915096cc9fb0f5c7df45bda7d25d5bf2b0506dd
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-09-10 09:07:07 +0900

    refactor: Footer UI 수정

--- src/app/common/Footer.tsx
+++ src/app/common/Footer.tsx
@@ -1,37 +1,35 @@
 'use client';
 import React from 'react';
 import { useRouter } from 'next/navigation';
+import { Divider, Instagram } from 'public';
 
 export const Footer = () => {
   const router = useRouter();
   return (
-    <div className="flex h-52 flex-col space-y-2.5 bg-[#f9f9f9] px-5 py-9 text-[11px] text-[#8f8f8f]">
-      <p className="text-[#666666]">참숯한우천국</p>
-      <div className="space-y-2.5">
-        <p>
-          대표: <span className="text-[#666666]">김선창</span>
-        </p>
-        <p>
-          주소:
-          <span className="text-[#666666]">
-            경기도 부천시 원미구 상이로21번길 26-15, 1층
-          </span>
-        </p>
-        <p>
-          사업자 등록 번호: <span className="text-[#666666]">488-28-01237</span>
-        </p>
-        <p>
-          개인정보담당: <span className="text-[#666666]">tisckd@naver.com</span>
-        </p>
+    <div className="flex justify-between space-x-1 bg-gy-200 pb-[23px] pl-4 pr-6 pt-[15px]">
+      <div className="flex flex-col space-y-[3px]">
+        <div className="flex flex-row items-center space-x-1">
+          <button
+            className="cursor-pointer p-2 text-gy-700 font-b2-normal-semibold"
+            onClick={() => router.push('/policy/privacy')}
+          >
+            개인정보 처리방침
+          </button>
+          <Divider />
+          <button
+            className="cursor-pointer p-2 text-gy-700 font-b2-normal-semibold"
+            onClick={() => router.push('/policy/service')}
+          >
+            이용약관
+          </button>
+        </div>
+        <div className="ml-2 text-gy-400 font-c1-medium">
+          © 2024 LUPG. All Rights Reserved.
+        </div>
       </div>
-      <div>
-        <button
-          className="cursor-pointer font-semibold text-[#666666]"
-          onClick={() => router.push('/policy/privacy')}
-        >
-          개인정보 처리방침
-        </button>
-      </div>
+      <button onClick={() => alert('인스타그램으로 이동합니다!')}>
+        <Instagram />
+      </button>
     </div>
   );
 };
