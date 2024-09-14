fa6a34c69504f179811a0f302cf1228cf6d2e846
chasyuss <cotn963@gmail.com>
2024-09-14 11:22:36 +0900

    style: 주점 상세 ui 변경

---
+++ src/app/(route)/pub/[id]/_components/DetailTitle.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { PageHeader } from '@/app/common/PageHeader';
+
+interface DetailTitleProps {
+  pubName: string;
+  oneLiner: string;
+  instagramUrl: string;
+  waitingCount: number;
+}
+
+const DetailTitle: React.FC<DetailTitleProps> = ({
+  pubName,
+  oneLiner,
+  instagramUrl,
+  waitingCount,
+}) => (
+  <div className="flex flex-col items-center justify-center gap-2 py-4">
+    <div className="flex text-gy-700 font-b2-normal-semibold">
+      👥 현재&nbsp;<span className="text-red-400">{waitingCount}</span>명 대기
+      중
+    </div>
+    <h1 className="text-gy-900 font-h1">{pubName}</h1>
+    <p className="text-gy-700 font-b1-normal-medium">{oneLiner}</p>
+    <a
+      href={instagramUrl}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex items-center justify-center border-b border-gy-500 text-gy-500 font-b2-normal-medium"
+    >
+      @인스타그램 바로가기
+    </a>
+  </div>
+);
+
+export default DetailTitle;
--- src/app/(route)/pub/[id]/page.tsx
+++ src/app/(route)/pub/[id]/page.tsx
@@ -3,7 +3,9 @@ import React, { useState, useEffect, useRef } from 'react';
 import { usePathname } from 'next/navigation';
 import WaitingTeams from '../_components/WaitingTeams';
 import { getPubInfoForUser } from '@/app/api/service/user/getPubInfoForUser';
-import { PageHeader } from '@/app/common/PageHeader';
+import DetailTitle from './_components/DetailTitle';
+import DetailMenuList from './_components/DetailMenuList';
+import DetailImage from './_components/DetailImage';
 
 interface Store {
   pub: {
@@ -95,102 +97,30 @@ const StoreDetailPage: React.FC = () => {
 
   return (
     <div className="min-w-screen flex min-h-screen flex-col">
-      <div className="relative h-64 w-full sm:h-80 md:h-96">
-        <div className="relative h-full w-full overflow-hidden">
-          <div
-            className="absolute flex h-full w-full transition-transform duration-1000 ease-in-out"
-            style={{
-              transform: `translateX(-${currentImageIndex * 100}%)`,
-            }}
-          >
-            {store.pub.thumbnails.length > 0 ? (
-              store.pub.thumbnails.map((thumbnail, index) => (
-                <img
-                  key={index}
-                  src={thumbnail}
-                  alt={`Slide ${index}`}
-                  className="w-full flex-shrink-0 object-cover"
-                />
-              ))
-            ) : (
-              <img
-                src="/images/place.png"
-                alt="Place"
-                className="w-full flex-shrink-0 object-cover"
-              />
-            )}
-          </div>
-          <PageHeader isDetailPage={true} />
-        </div>
+      <div className="relative h-full w-full ">
+        <DetailImage thumbnails={store.pub.thumbnails} />
       </div>
-      <div className="mx-auto w-full max-w-2xl flex-1 overflow-auto rounded-bl-[40px] rounded-br-[40px] border bg-white p-6">
-        <div>
-          <div className="mt-2 flex w-80 items-start justify-start bg-white pl-0.5 text-xs font-bold">
-            현재&nbsp;
-            <span style={{ color: '#FF805A' }}>{store.pub.waitingCount}</span>
-            명이 대기하고 있어요
-          </div>
-          <h1 className="mb-2 text-2xl font-bold">{store.pub.pubName}</h1>
-          <p className="text-sm">{store.pub.oneLiner}</p>
-          <div className="mt-3 flex items-center">
-            <div
-              className={`items-center justify-center rounded-full px-2.5 py-1 text-[8px] font-medium text-white ${store.pub.studentCard ? 'bg-[#E87567]' : 'bg-[#3B4D9B]'}`}
-            >
-              {store.pub.studentCard ? '학생증 필요' : '학생증 불필요'}
-            </div>
-          </div>
-        </div>
-        <div className="mt-5 flex h-16 flex-row border-b border-[#EFEFF0]">
-          <a
-            href={store.pub.instagramUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mb-2 flex w-1/2 items-center justify-center border-r border-gray-300 text-lg"
-          >
-            인스타그램
-          </a>
-          <div
-            className="mb-2 flex w-1/2 cursor-pointer items-center justify-center text-lg"
-            onClick={scrollToMenu}
-          >
-            메뉴
-          </div>
-        </div>
-        <div ref={menuRef} className="mb-20 mt-5 w-full px-4">
-          <div className="mb-5 mt-5 text-2xl font-bold">메뉴</div>
-          <div className="flex flex-col">
-            {store.menu.length === 0 ? (
-              <p className="text-center text-gray-600">
-                현재 등록되어있는 메뉴가 없습니다
-              </p>
-            ) : (
-              store.menu.map((menuItem: MenuItem, index) => (
-                <div
-                  key={menuItem.menuName}
-                  className={`mb-4 ${
-                    index !== store.menu.length - 1
-                      ? 'border-b-[0.7px] border-[#C2C2C2]'
-                      : ''
-                  }`}
-                >
-                  <h2 className="text-xl font-bold">{menuItem.menuName}</h2>
-                  <p className="mb-3 mt-1 text-sm">{menuItem.price}원</p>
-                  <p className="mb-5 text-sm text-[#7D7D7D]">
-                    {menuItem.oneLiner}
-                  </p>
-                </div>
-              ))
-            )}
-          </div>
-        </div>
-        <nav className="fixed bottom-0 left-0 right-0 border-none bg-transparent mobile:mx-auto ">
-          <nav className="mx-auto flex w-full max-w-[31rem] justify-evenly ">
+
+      <div className="mx-auto w-full flex-1 overflow-auto bg-wt">
+        <DetailTitle
+          pubName={store.pub.pubName}
+          oneLiner={store.pub.oneLiner}
+          instagramUrl={store.pub.instagramUrl}
+          waitingCount={store.pub.waitingCount}
+        />
+        <div className="border bg-gy-100" />
+
+        <div ref={menuRef} />
+        <DetailMenuList menu={store.menu} />
+
+        <nav className="fixed bottom-0 left-0 right-0 mx-auto w-full max-w-[480px] border-none bg-transparent px-4">
+          <div className="flex items-center justify-center">
             <WaitingTeams
               pubId={parseInt(id as string, 10)}
               studentCard={store.pub.studentCard}
               openStatus={store.pub.openStatus}
             />
-          </nav>
+          </div>
         </nav>
       </div>
     </div>
