fb9c31638062554d522e61cdca4d09e7367673ff
chasyuss <cotn963@gmail.com>
2024-09-22 14:01:46 +0900

    feat: searchbar에서 close 버튼 추가

--- src/app/(route)/(home)/_components/LandingSearchBar.tsx
+++ src/app/(route)/(home)/_components/LandingSearchBar.tsx
@@ -1,18 +1,24 @@
 'use client';
 import React from 'react';
-import { LogoCircle, Search, User } from 'public';
+import { Close, LogoCircle, Search, User } from 'public';
 import { useRouter } from 'next/navigation';
 
 interface LandingSearchBarProps {
   searchTerm: string;
   onSearchChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  setSearchTerm: (value: string) => void;
 }
 
 const LandingSearchBar: React.FC<LandingSearchBarProps> = ({
   searchTerm,
   onSearchChange,
+  setSearchTerm,
 }) => {
   const router = useRouter();
+
+  const clearSearch = () => {
+    setSearchTerm('');
+  };
 
   return (
     <div className="flex w-full items-center justify-between gap-3 p-4">
@@ -23,15 +29,22 @@ const LandingSearchBar: React.FC<LandingSearchBarProps> = ({
         />
       </div>
 
-      <div className="flex w-full max-w-[270px] flex-grow items-center rounded-full bg-gy-100 py-2 pl-4 pr-6">
+      <div className="relative flex w-full max-w-[270px] flex-grow items-center rounded-full bg-gy-100 py-2 pl-4 pr-6">
         <Search className="mr-2 text-gy-400" />
         <input
           type="text"
-          className="placeholder:text-font-b1-normal-medium w-full border-none bg-gy-100 outline-none placeholder:text-gy-400"
+          className="placeholder:text-font-b1-normal-medium w-full border-none bg-gy-100 pr-2 outline-none placeholder:text-gy-400"
           placeholder="주점을 검색해보세요."
           value={searchTerm}
           onChange={onSearchChange}
         />
+
+        {searchTerm && (
+          <Close
+            className="absolute right-2 cursor-pointer"
+            onClick={clearSearch}
+          />
+        )}
       </div>
 
       <div className="flex-shrink-0">
