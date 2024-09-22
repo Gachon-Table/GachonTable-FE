948ecd860d0ccfc76e7a559b77680bb7f065af89
chasyuss <cotn963@gmail.com>
2024-09-22 14:45:30 +0900

    style: 아이콘 추가

--- public/index.ts
+++ public/index.ts
@@ -18,3 +18,4 @@ export { default as Search } from './icons/search-icon.svg';
 export { default as Instagram } from './icons/instagram.svg';
 export { default as Home } from './icons/home.svg';
 export { default as BackButtonWT } from './icons/back-button-wt.svg';
+export { default as Close } from './icons/close-icon.svg';
