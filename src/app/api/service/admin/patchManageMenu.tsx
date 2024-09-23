2ac63464282a7d8acf2e9b16341a14f96e508338
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-09-24 00:55:18 +0900

    feat: patchManageMenu api 함수 구현

---
+++ src/app/api/service/admin/patchManageMenu.tsx
@@ -0,0 +1,29 @@
+import adminAxios from '@/app/api/axios/adminAxios';
+
+interface MenuRequest {
+  menuId?: number | undefined;
+  menuName: string;
+  price: string;
+  oneLiner: string;
+  thumbnail: string;
+}
+
+interface UpdatedPubData {
+  thumbnails: string[];
+  menuRequests: MenuRequest[];
+}
+
+export const patchManageMenu = async (UpdatedPubData: UpdatedPubData) => {
+  try {
+    const response = await adminAxios.patch('/manage', UpdatedPubData);
+    if (response.status === 200) {
+      console.log('정보 업데이트 성공');
+      alert('저장되었습니다.');
+      return response.data;
+    }
+    return response;
+  } catch (error) {
+    console.error('호출 실패: ', error);
+    throw error;
+  }
+};
