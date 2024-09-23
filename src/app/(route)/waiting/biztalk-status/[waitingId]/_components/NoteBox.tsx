3315cbc7488dcb4b1cf0d91c9850583c693a7dd4
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-09-24 03:48:28 +0900

    style: NoteBox 컴포넌트 UI 구현 및 페이지에 적용

---
+++ src/app/(route)/waiting/biztalk-status/[waitingId]/_components/NoteBox.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const NoteBox = () => {
+  return (
+    <div className="flex w-[382px] flex-col space-y-2 rounded-md border border-gy-200 pb-[26px] pl-4  pt-4">
+      <div className="text-gy-600 font-c1-semibold">유의사항</div>
+      <ul className="list-disc space-y-2 pl-4 text-gy-900 font-c1-semibold">
+        <li>
+          대기 접수 후 <span className="text-red-400">알림톡</span>을 꼭
+          확인해주세요.
+        </li>
+        <li>
+          시간 내에 입장하지 않으면{' '}
+          <span className="text-red-400">자동으로 취소</span> 처리됩니다.
+        </li>
+        <li>
+          빠른 입장을 위해{' '}
+          <span className="text-red-400">재학생 인증 수단 및 주민등록증</span>을
+          준비해주세요.
+        </li>
+      </ul>
+    </div>
+  );
+};
