9f43ccd7a19621c2355ead51e120d467a85119f0
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-09-25 00:41:06 +0900

    feat: api 호출 후 새로고침 로직 구현

--- src/app/(route)/admin/_components/client-management/ServedClientItem.tsx
+++ src/app/(route)/admin/_components/client-management/ServedClientItem.tsx
@@ -1,21 +1,34 @@
 import React from 'react';
 
 export interface ServedClientItemProps {
-  tableNum?: number;
+  seatingId?: number;
+  seatingNum?: number;
+  tableType?: 'BASIC' | 'PARTY';
   exitTime?: string;
   waitingId?: string;
+  handleExitModal?: () => void;
 }
 
 export const ServedClientItem = ({
-  tableNum,
+  seatingNum,
   exitTime,
+  handleExitModal,
 }: ServedClientItemProps) => {
   return (
-    <div className="shadow-client-item border-gy-100 flex flex-col space-y-2.5 rounded-lg border bg-wt p-5">
-      <div className="font-h4">{tableNum}번 테이블</div>
-      <div className="text-gy-600 border-gy-200 rounded-md border bg-wt px-[125.5px] py-[13px] font-b2-normal-semibold">
-        퇴장 시간 {exitTime}
+    <div className="flex flex-col space-y-3 rounded-lg border border-gy-100 bg-wt p-5 shadow-client-item">
+      <div className="flex flex-row items-center space-x-1 font-h4">
+        <div className="text-gy-800">{seatingNum}번 테이블</div>
+        <div className="text-gy-200 font-b1-normal-semibold">|</div>
+        <div className=" bg-wt text-gy-500">퇴장 예정 시간 {exitTime}</div>
       </div>
+      <button
+        className="w-[342px] rounded-md bg-primary-200 px-[117px] py-[13px] text-primary-400"
+        onClick={handleExitModal}
+      >
+        <span className="block w-[108px] text-center font-b2-normal-semibold">
+          퇴장 처리하기
+        </span>
+      </button>
     </div>
   );
 };
