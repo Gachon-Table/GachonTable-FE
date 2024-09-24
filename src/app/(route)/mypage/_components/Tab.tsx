24752bca27bf7671fd04de0e7039a3e121770460
chasyuss <cotn963@gmail.com>
2024-09-24 13:03:02 +0900

    style: 마이페이지 tab 색상 변경

--- src/app/(route)/mypage/_components/Tab.tsx
+++ src/app/(route)/mypage/_components/Tab.tsx
@@ -9,7 +9,7 @@ const Tab = ({ curTab, setFunc }: TabProps) => {
   return (
     <div className="mx-4 mt-4 flex justify-between">
       <div
-        className={`flex w-[50%] cursor-pointer flex-col items-center border-b-2 p-2 ${curTab === 'ing' ? `text-text-gy-900 border-black` : `border-gy-200 text-gy-400`}`}
+        className={`flex w-[50%] cursor-pointer flex-col items-center border-b-2 p-2 ${curTab === 'ing' ? `border-gy-900 text-gy-900` : `border-gy-200 text-gy-400`}`}
         onClick={() => {
           setFunc('ing');
         }}
@@ -17,7 +17,7 @@ const Tab = ({ curTab, setFunc }: TabProps) => {
         <div className="font-b1-normal-semibold">줄서기 현황</div>
       </div>
       <div
-        className={`flex w-[50%] cursor-pointer flex-col items-center border-b-2 p-2 ${curTab === 'ed' ? `text-text-gy-900 border-black` : `border-gy-200 text-gy-400`}`}
+        className={`flex w-[50%] cursor-pointer flex-col items-center border-b-2 p-2 ${curTab === 'ed' ? `border-gy-900 text-gy-900` : `border-gy-200 text-gy-400`}`}
         onClick={() => {
           setFunc('ed');
         }}
