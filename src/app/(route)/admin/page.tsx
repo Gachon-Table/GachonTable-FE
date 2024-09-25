db86792b9f7e952dbaa733d8039ee0dfbd93103c
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-09-25 13:45:57 +0900

    feat: 로그인 성공에 대한 로직 구현

--- src/app/(route)/admin/page.tsx
+++ src/app/(route)/admin/page.tsx
@@ -3,10 +3,12 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { adminLogin, isAuthenticated } from '@/app/api/service/admin/adminAuth';
 import { Logo } from 'public';
+import { ToastModal } from '@/app/common/ToastModal';
 
 export default function AdminLogin() {
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoginSuccess, setIsLoginSuccess] = useState(false);
   const router = useRouter();
 
   const handleLogin = async () => {
@@ -18,6 +20,10 @@ export default function AdminLogin() {
     const loginSuccess = await adminLogin(credentials);
     if (loginSuccess) {
       router.push('/admin/client-management');
+      setIsLoginSuccess(true);
+      setTimeout(() => {
+        setIsLoginSuccess(false);
+      }, 2000);
     }
   };
 
@@ -39,55 +45,65 @@ export default function AdminLogin() {
   }, [router]);
 
   return (
-    <div className=" flex min-h-screen flex-col items-center justify-between bg-white">
-      <div className="tablet:max-w-md laptop:max-w-lg desktop:max-w-xl mt-36 w-full max-w-xs mobile:max-w-sm">
-        <div className="mb-[53px] flex flex-col items-center">
+    <div className="flex h-screen flex-col bg-white">
+      <div className="mt-[157px]">
+        <div className="mb-[54px] flex flex-col items-center">
           <Logo />
-          <div className="tablet:text-3xl laptop:text-4xl desktop:text-5xl mt-9 text-center text-2xl font-bold text-gy-700">
+          <div className="mt-6 text-center text-gy-700 font-h1">
             관리자 로그인
           </div>
         </div>
 
-        <div className="mb-6">
-          <label
-            htmlFor="id"
-            className="mb-[6px] block text-sm font-medium text-gy-600"
-          >
-            아이디
-          </label>
-          <input
-            id="id"
-            type="text"
-            placeholder="아이디를 입력해주세요"
-            value={id}
-            onChange={(e) => setId(e.target.value)}
-            className="w-full rounded-md border border-gy-0 bg-[#F4F4F4] py-4 pl-4 pr-[71px] focus:border-gy-0 focus:outline-none focus:ring-2"
-          />
-        </div>
-        <div>
-          <label
-            htmlFor="password"
-            className="mb-[6px] block text-sm font-medium text-gy-600"
-          >
-            비밀번호
-          </label>
-          <input
-            id="password"
-            type="password"
-            placeholder="비밀번호를 입력해주세요"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full rounded-md border border-gy-0 bg-[#F4F4F4] py-4 pl-4 pr-[71px] focus:border-gy-0 focus:outline-none focus:ring-2"
-          />
+        <div className="flex flex-col items-center space-y-6">
+          <div className="space-y-[6px]">
+            <label
+              htmlFor="id"
+              className="block text-gy-600 font-b1-normal-medium"
+            >
+              아이디
+            </label>
+            <input
+              id="id"
+              type="text"
+              placeholder="아이디를 입력해주세요."
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+              className="placeholder:text-font-b1-normal-medium w-[382px] rounded-md bg-gy-0 py-4 pl-4 pr-[71px] focus:outline-none"
+            />
+          </div>
+          <div className="space-y-[6px]">
+            <label
+              htmlFor="password"
+              className="block text-gy-600 font-b1-normal-medium"
+            >
+              비밀번호
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="비밀번호를 입력해주세요."
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="placeholder:text-font-b1-normal-medium w-[382px] rounded-md bg-gy-0 py-4 pl-4 pr-[71px] focus:outline-none"
+            />
+          </div>
         </div>
       </div>
 
-      <div className="tablet:max-w-md laptop:max-w-lg desktop:max-w-xl mb-8 w-full max-w-xs mobile:max-w-sm">
+      {isLoginSuccess && (
+        <div className="mt-[206px] flex justify-center">
+          <ToastModal message={'로그인이 완료되었습니다!'} />
+        </div>
+      )}
+
+      <div className="fixed bottom-8 left-0 right-0 flex justify-center">
         <button
           onClick={handleLogin}
-          className="h-[64px] w-full rounded-md bg-primary-400 px-6 py-[22px] text-lg font-bold leading-6 text-white"
+          className="flex w-[382px] items-center justify-center rounded-md bg-primary-400 px-6 py-[19px]"
         >
-          로그인
+          <span className="block w-[334px] text-center text-wt font-h4">
+            로그인
+          </span>
         </button>
       </div>
     </div>
