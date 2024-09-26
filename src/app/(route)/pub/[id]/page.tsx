e7e6e994ce67d0922b96ac61592c2cab2667d3d8
chasyuss <cotn963@gmail.com>
2024-09-26 19:05:57 +0900

    feat: 주점 상세 이미지가 전부 스크롤 처리 되면 detailimage 사라지게 수정

--- src/app/(route)/pub/[id]/page.tsx
+++ src/app/(route)/pub/[id]/page.tsx
@@ -7,6 +7,8 @@ import { getPubInfoForUser } from '@/app/api/service/user/getPubInfoForUser';
 import DetailTitle from './_components/DetailTitle';
 import DetailMenuList from './_components/DetailMenuList';
 import DetailImage from './_components/DetailImage';
+import { PageHeader } from '@/app/common/PageHeader';
+import { BackButton } from 'public';
 
 interface Store {
   pub: {
@@ -35,11 +37,14 @@ const StoreDetailPage: React.FC = () => {
   const pathname = usePathname();
   const id = pathname.split('/').pop();
   const menuRef = useRef<HTMLDivElement>(null);
+  const imageRef = useRef<HTMLDivElement>(null);
 
   const [store, setStore] = useState<Store | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isImageVisible, setIsImageVisible] = useState(true);
 
   useEffect(() => {
     const fetchStoreData = async () => {
@@ -48,8 +53,8 @@ const StoreDetailPage: React.FC = () => {
         setStore(response);
         console.log(response);
       } catch (err) {
-        console.error('Error fetching store data:', err);
-        setError('An error occurred while fetching data.');
+        console.error('데이터를 가져오지 못했습니다', err);
+        setError('데이터를 가져오지 못했습니다');
       } finally {
         setLoading(false);
       }
@@ -58,11 +63,26 @@ const StoreDetailPage: React.FC = () => {
     fetchStoreData();
   }, [id]);
 
-  const scrollToMenu = () => {
-    if (menuRef.current) {
-      menuRef.current.scrollIntoView({ behavior: 'smooth' });
+  const handleScroll = () => {
+    if (imageRef.current && menuRef.current) {
+      const imageBottom = imageRef.current.getBoundingClientRect().bottom;
+      const menuTop = menuRef.current.getBoundingClientRect().top;
+
+      setIsImageVisible(imageBottom > 0);
+      setIsScrolled(menuTop <= window.innerHeight);
+
+      if (imageBottom > 0) {
+        setIsScrolled(false); // 이미지 보일거면 스크롤 false
+      }
     }
   };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   useEffect(() => {
     if (store && store.pub.thumbnails.length > 1) {
@@ -100,9 +120,24 @@ const StoreDetailPage: React.FC = () => {
 
   return (
     <div className="min-w-screen flex min-h-screen flex-col">
-      <div className="relative h-full w-full ">
+      <div
+        className={`relative w-full transition-opacity duration-300 ${
+          isImageVisible ? 'opacity-100' : 'opacity-0'
+        }`}
+        ref={imageRef}
+      >
         <DetailImage thumbnails={store.pub.thumbnails} />
       </div>
+
+      {isScrolled && (
+        <div className="sticky top-0 z-10 bg-wt">
+          <PageHeader
+            icon={<BackButton />}
+            isDetailPage={false}
+            title={store.pub.pubName}
+          />
+        </div>
+      )}
 
       <div className="mx-auto w-full flex-1 overflow-auto bg-wt">
         <DetailTitle
