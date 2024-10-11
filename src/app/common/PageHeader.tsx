33cf6c64dd235c4c9f3d51d23ba916cf2b483356
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-10-12 02:30:00 +0900

    fix: 왼쪽 버튼 클릭되지 않는 오류 수정

--- src/app/common/PageHeader.tsx
+++ src/app/common/PageHeader.tsx
@@ -7,36 +7,30 @@ interface PageHeaderProps {
   icon?: React.ReactNode;
   title?: string;
   isDetailPage?: boolean;
-  clickHandler?: () => void;
 }
 
 export const PageHeader = ({
   icon = <BackButton />,
   title,
-  isDetailPage,
-  clickHandler,
+  isDetailPage = false,
 }: PageHeaderProps) => {
   const router = useRouter();
 
   const handleClick = () => {
-    if (window.history.length > 1) {
-      router.back();
-    } else {
-      router.push('/');
-    }
+    router.push('/');
   };
 
   return (
     <div
       className={`z-99999 relative flex w-full items-center px-4 py-3 ${isDetailPage ? 'absolute bg-none' : 'bg-wt'}`}
     >
-      <div
-        onClick={clickHandler || handleClick}
-        className="flex h-8 w-8 cursor-pointer items-center justify-center"
+      <button
+        onClick={handleClick}
+        className="relative z-10 flex h-8 w-8 cursor-pointer items-center justify-center"
       >
         {icon}
-      </div>
-      <div className="absolute left-0 right-0 text-center font-b1-normal-semibold">
+      </button>
+      <div className="absolute left-0 right-0 z-0 text-center font-b1-normal-semibold">
         {title}
       </div>
     </div>
