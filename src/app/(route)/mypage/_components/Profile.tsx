d1707bde84cc6db2c99626a48331561ad418d0c5
chasyuss <cotn963@gmail.com>
2024-10-27 21:46:02 +0900

    fix: 로그인 mb 조정

--- src/app/(route)/mypage/_components/Profile.tsx
+++ src/app/(route)/mypage/_components/Profile.tsx
@@ -12,7 +12,7 @@ const Profile = () => {
   };
   return (
     <div className="flex items-center justify-center px-4">
-      <div className="p-safe-bottom flex w-[382px] flex-col  gap-[10px] text-gy-400 font-b1-normal-medium">
+      <div className="flex w-[382px] flex-col  gap-[10px] text-gy-400 font-b1-normal-medium">
         <button
           className="flex h-[64px] w-full cursor-pointer items-center justify-center rounded-[6px] bg-kakao px-6 py-[22px] text-bk font-h4"
           onClick={loginProcess}
