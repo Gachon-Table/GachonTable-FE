35bea4b54ab1335dcbc12d433ee391762edb8a1b
chasyuss <cotn963@gmail.com>
2024-10-31 10:58:36 +0900

    fix: 카카오 로그인 토큰 받아오는 로직 수정

--- src/app/(route)/oauth/page.tsx
+++ src/app/(route)/oauth/page.tsx
@@ -11,22 +11,36 @@ const Oauth = () => {
 
   useEffect(() => {
     const loginProcess = async () => {
-      const result = await axios.post(
-        `${process.env.NEXT_PUBLIC_API_URL}/login`,
-        null,
-        { params: { code: code } },
-      );
-      localStorage.setItem('userAccessToken', result.data.accessToken);
-      localStorage.setItem('userRefreshToken', result.data.refreshToken);
-      localStorage.setItem('userName', result.data.username);
-      const callbackPath = localStorage.getItem('callbackPath');
-      if (callbackPath) {
-        router.push(callbackPath);
+      try {
+        const result = await axios.post(
+          `${process.env.NEXT_PUBLIC_API_URL}/login`,
+          null,
+          { params: { code: code } },
+        );
+
+        localStorage.setItem('userAccessToken', result.data.accessToken);
+        localStorage.setItem('userRefreshToken', result.data.refreshToken);
+        localStorage.setItem('userName', result.data.username);
+
+        const callbackPath = localStorage.getItem('callbackPath');
+        if (callbackPath) {
+          router.push(callbackPath);
+        }
+      } catch (error) {
+        console.error('로그인 에러 발생', error);
+
+        if (error.response && error.response.data.error_code === 'KOE320') {
+          const CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
+          const REDIRECT_URI = `${window.location.protocol}//${window.location.host}/oauth`;
+
+          const codeRequestURL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
+          window.location.href = codeRequestURL;
+        }
       }
     };
 
-    loginProcess();
-  }, [code]);
+    if (code) loginProcess();
+  }, [code, router]);
 
   return <div></div>;
 };
